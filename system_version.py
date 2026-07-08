90beb01951292f0e2cc84b7dd5af6f5712fc8f8d
Robin <whfubin@gmail.com>
2026-07-07 18:21:19 -0600

    Register config validation in system checks

--- system_version.py
+++ system_version.py
@@ -27,6 +27,7 @@ CORE_MODULES = [
 
 
 VALIDATION_MODULES = [
+    "validate_config.py",
     "validate_backtest_outputs.py",
     "validate_portfolio_outputs.py",
     "validate_position_sizing_outputs.py",
